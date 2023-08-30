7a9eab0487c4beeb5c2004573c40776430d3e941
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-08-30 11:07:37 -0400

    Update collatz_analyse.py

--- TD1/collatz/collatz_analyse.py
+++ TD1/collatz/collatz_analyse.py
@@ -32,7 +32,26 @@ plt.show()
 #### QUESTION C 
 # la méthode hist de matplotlib.pyplot est capable seule de créer un histogramme directement
 plt.hist(iterations_collatz) 
-plt.show() 
 
+
+##m.thode manuelle 
+freq = {}
+for iter in iterations_collatz: 
+    # on va chercher l'élément clé d'une itération avec freq[iter]
+    # on on trouve le sa clé (#itérations)correspondante avec .get
+    # si get ne trouve rien il retourne 0 (l'élément n'a pas encore été comptabilisé)
+    # on rajoute 1 pour signifier qu'on ajoute une itération 
+    freq[iter] = freq.get(iter, 0) + 1 
+# Mettons le dictionnaire en ordre croissant de clés maintenant
+freq = dict(sorted(freq.items()))
+# Maintenant organisons le dict en 2 listes correspondantes 
+domaine_hist = []
+image_hist = []
+for (x,y) in freq.items(): 
+    domaine_hist.append(x) 
+    image_hist.append(y) 
+fig, ax = plt.subplots() 
+ax.bar(domaine_hist, image_hist)
+plt.show() 
 #Correction
 pytest.main(['-q', '--tb=long', 'collatz_corr.py'])
