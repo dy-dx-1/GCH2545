c66797fafb4499567e237ff5fdd68fadad71da34
dy-dx-1 <nic19sarbe@gmail.com>
2023-10-18 20:46:59 -0400

    analyse working, need formatting graphs only

--- TD5/lorenz/lorenz_analyse.py
+++ TD5/lorenz/lorenz_analyse.py
@@ -24,13 +24,26 @@ class parametres():
 prm = parametres()
 
 # Conditions initiales
-
+cond_i = np.array([10, 10, 20])
+t_sim = 30 # temps simulation en s 
+dt = 10e-3 # pas de temps en s 
 
 # Appel de la fonction rk4
-
+sols, temps = rk4(cond_i, dt, t_sim, prm) 
+x_vals = sols[:,0]
+y_vals = sols[:,1]
+z_vals = sols[:,2]
 
 # Graphiques
+ax1 = plt.figure().add_subplot(projection='3d') 
+ax2 =plt.figure().add_subplot(projection='3d') 
 
+ax1.plot(x_vals, y_vals, z_vals, 'r-') 
+
+# génération du graphique avec condi légèrement perturbées 
+sols, temps = rk4(cond_i+10e-4, dt, t_sim, prm) 
+x_vals, y_vals, z_vals = sols[:,0], sols[:,1], sols[:,2] 
+ax2.plot(x_vals, y_vals, z_vals, 'y-')
 
 plt.show()
 
