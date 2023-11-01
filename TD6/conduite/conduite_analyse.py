33b3c61417a5d41e7796cb8b2c252df49ee5d210
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-11-01 12:06:29 -0400

    R4R

--- TD6/conduite/conduite_analyse.py
+++ TD6/conduite/conduite_analyse.py
@@ -20,24 +20,36 @@ except:
 # Assignation des paramètres
 # ATTENTION! Ne pas changer le nom des attributs, seulement les valeurs
 class parametres():
-    Tr = 0              # Température à l'intérieur de la conduite
-    Te = 0              # Température ambiante autour de la conduite
+    Tr = 70              # Température à l'intérieur de la conduite
+    Te = 25              # Température ambiante autour de la conduite
     k = 0               # [W*m^-1*K^-1] Conductivité thermique
-    h = 0               # [W*m^-2*K^-1] Coefficient de convection thermique
-    Re = 0              # [m] Rayon externe
-    Ri = 0              # [m] Rayon interne
-    n = 0               # [-] Nombre de noeuds
+    h = 12               # [W*m^-2*K^-1] Coefficient de convection thermique
+    Re = 4              # [m] Rayon externe
+    Ri = 2              # [m] Rayon interne
+    n = 10              # [-] Nombre de noeuds
     dr = (Re-Ri)/(n-1)  # [-] Pas en espace
 prm = parametres()
 
 # Coefficients de conductivité themique
 k = [1.3,50,200]
+prm.k = 1.3 
 
 # Méthode de différences finies
-
+pos, temp_beton = mdf(prm) 
+prm.k = 50
+pos, temp_acier = mdf(prm) 
+prm.k = 200 
+pos, temp_alu = mdf(prm) 
 
 # Graphique
-
+plt.plot(pos, temp_beton, 'r.-', label="Béton") 
+plt.plot(pos, temp_acier, 'g.-', label="Acier") 
+plt.plot(pos, temp_alu, "b.-", label="Aluminium") 
+plt.title("Distribution de température radiale avec 10 noeuds") 
+plt.xlabel("Position radiale") 
+plt.ylabel("Température en Celcius") 
+plt.legend() 
+plt.grid() 
 
 plt.show()
 
--- TD6/conduite/conduite_fct.py
+++ TD6/conduite/conduite_fct.py
@@ -19,7 +19,35 @@ def mdf(prm):
         - Vecteur (array) de dimension N composé de la position radiale à laquelle les températures sont calculées, où N le nombre de noeuds.
         - Vecteur (array) de dimension N composé de la température en fonction du rayon, où N le nombre de noeuds
     """
+    # unpackons nos paramètres 
+    Tr, Te, k, h, Re, Ri, n, dr = prm.Tr, prm.Te, prm.k, prm.h, prm.Re, prm.Ri, prm.n, prm.dr 
+    # Préparons les vecteurs nécéssaires au calcul  
+    pos_r = np.linspace(Ri, Re, n) # linspace divisera notre domaine radial en n noeuds 
+    matrice_diff = np.zeros((n,n))
+    vect_residu = np.zeros((n,1))
+    # Ajoutons la condition initiale de Dirichlet 
+    matrice_diff[0,0] = 1 # coefficient 1 pour notre To 
+    vect_residu[0,0] = Tr  # Solution associée à To = Tr
 
-    # Fonction à écrire
+    # Promenons nous de noeud en noeud pour générer les lignes de notre matrice 
+    for i in range(1, n-1):  # on saute la première et dernière étape car on utilisera les conditions initiales  
+        # Évaluons coefficient de Ti+1, Ti et Ti-1 avant de les mettre dans matrice 
+        Tnext = (pos_r[i] / (dr**2)) + (1/(2*dr))
+        Ti = (-2*pos_r[i]) / (dr**2) 
+        Tprev = (pos_r[i] / (dr**2)) - (1/(2*dr))
+        # Selon relation que j'ai trouvé en dessinant sur ma tablette, je remplis la matrice des diff finies avec les coefficients respectifs
+        # Pour un i donné, coeff de élément i va à pos [i, i] ; élément i+1 va à [i, i+1] ; élément i-1 va à [i, i-1] 
+        matrice_diff[i, i-1] = Tprev 
+        matrice_diff[i, i] = Ti 
+        matrice_diff[i, i+1] = Tnext 
+        # Le reste reste des zéros  
 
-    return # à compléter
+    # Ensuite ajoutons notre condition de robin 
+    matrice_diff[n-1, n-1] = 3+(2*h*dr/k) # on indexe comme si notre i est n-1 pour cette condition 
+    matrice_diff[n-1, n-2] = -4 
+    matrice_diff[n-1, n-3] = 1 
+
+    vect_residu[n-1, 0] = 2*h*dr*Te / k 
+    # Maintenant résolvons le sys matricielle pour trouver nos [T0, T1, T2, ..., Tn] 
+    temperatures = np.linalg.solve(matrice_diff, vect_residu)
+    return pos_r, temperatures
