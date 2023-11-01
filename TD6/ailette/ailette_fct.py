c3f827c5345393b5a6c752feaee5eba23fd3b1a2
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-11-01 13:45:34 -0400

    début remplissage ailettes

--- TD6/ailette/ailette_fct.py
+++ TD6/ailette/ailette_fct.py
@@ -18,8 +18,31 @@ def mdf(prm):
         - Vecteur (np.array) donnant la température tout au long de l'ailette en Kelvin
         - Vecteur (np.array) donnant la position tout au long de l'ailette (axe z) en mètre
     """
+    # Tout d'abord on unpack les params 
+    L, D, k, T_a, T_w, h, n = prm.L, prm.D, prm.k, prm.T_a, prm.T_w, prm.h, prm.N 
+    # Préparons la matrice des différences finies & le vecteur résidu 
+    matrice = np.zeros((n,n)) 
+    residu = np.zeros((n, 1)) 
+    # Définissons le domaine de résolution 
+    dl = L/(n-1) # largeur des intervalles 
+    positions = np.linspace(0, L, n) 
+    # Ajoutons la condition de Dirichlet 
+    matrice[0,0] = 1 
+    residu[0,0] = T_w 
+    # Promenons nous parmi les index des noeuds pour remplir la matrice de coefficients 
+    for i in range(1, n-1): 
+        Tprev = None 
+        Ti = None 
+        Tnext = None 
+        matrice[i, i+1] = Tnext 
+        matrice[i, i] = Ti 
+        matrice[i, i-1] = Tprev 
+    # Maintenant rajoutons la condition de Neumann 
+    matrice[n-1, n-1] = None    # on indexe comme si notre i est n-1 pour cette condition 
+    matrice[n-1, n-2] = None
+    matrice[n-1, n-3] = None 
 
-    # Fonction à écrire
+    residu[n-1, 0] = None 
 
     return # à compléter
 
