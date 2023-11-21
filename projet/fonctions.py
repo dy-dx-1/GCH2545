0ff1b504933f6ef02ace13e65a07726d5ffbce43
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-11-21 14:52:50 -0500

    potentially fixed inverted matrix pos

--- projet/fonctions.py
+++ projet/fonctions.py
@@ -3,7 +3,13 @@ import numpy as np
 def psi_exact(r, theta, params): 
     return params.u_inf * r * np.sin(theta*(1-( (params.R**2)/(params.r**2) )))
 
-def convert_indices(nx:int, i = None, j = None, k = None)->int: 
+def gen_maille(r_o, r_max, theta_o, theta_max, nx, ny): 
+    """ 
+    Genère les coordonnées r et theta de chacun des points du maillage sous forme de tuples de coords 
+    """
+    maille = np.vstack([ [(r, theta) for r in np.linspace(r_o, r_max, nx)] for theta in np.linspace(theta_max, theta_max, ny) ])
+
+def convert_indices(ny:int, i = None, j = None, k = None)->int: 
     """
     Prend 2 indices et retourne celui qui n'est pas spécifié selon un maillage de forme 
 
@@ -12,10 +18,10 @@ def convert_indices(nx:int, i = None, j = None, k = None)->int:
     """
     # on assume que le programmeur sait utiliser la fonction donc pas de check complet des paramètres passés
     if i is None: 
-        return k - (nx*j) 
+        return k - (ny*j) 
     elif j is None: 
-        return (k-i) / nx 
+        return (k-i) / ny 
     elif k is None: 
-        return i + (j*nx)
+        return i + (j*ny)
     else: 
         return None 
