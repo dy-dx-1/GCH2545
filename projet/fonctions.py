1e8feff2b49f03e761b3835a9e1e89635526f39e
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-11-21 17:34:31 -0500

    framework pr maillage du centre

--- projet/fonctions.py
+++ projet/fonctions.py
@@ -31,3 +31,47 @@ def convert_indices(ny:int, i = None, j = None, k = None)->int:
         return i + (j*ny)
     else: 
         return None 
+    
+def gen_central_values(k, nx, ny): 
+    N = nx*ny 
+    mat_ref = np.zeros((N, N)) # on crée un matrice NxN qu'on remplira des coefficients 
+             # on sépare ceci de la matrice des noeuds de la fonction mdf pour faciliter nos tests, on devrait avoir une matrice dont toutes les valeurs des bords sont nulles 
+    Tk_ny = 0 
+    Tk_ny_ = 0 
+    Tk_1 = 0 
+    Tk_1_ = 0 
+    Tk = 0            
+    # Calculons la position i de reférence par rapport à k 
+    i = k%nx  # TODO check 
+    # Retrouvons avec k et i la position j du noeud 
+    j = convert_indices(ny, i=i, j=None, k=k)
+    # Plaçons les coefficients dans la matrice 
+    mat_ref[j, i] = Tk 
+    mat_ref[j, i+1] = Tk_1 
+    mat_ref[j, i-1] = Tk_1_
+    mat_ref[j+1, i] = Tk_ny 
+    mat_ref[j-1, i] = Tk_ny_
+    # Le résidu est nul pour ces noeuds donc on n'a pas besoin de le modifier  
+
+def mdf(r_min, r_max, theta_min, theta_max, nx, ny): 
+    """
+    Applique la méthode des différences finies pour résoudre le système sur le domaine 2D 
+    """
+    # Préparons la matrice qui acceuillera les différentes solutions des noeuds 
+    N = nx*ny 
+    noeuds = np.zeros((N,N)) 
+    # Préparons le vecteur résidu associé à la matrice des noeuds 
+    res = np.zeros((1,N))
+    # Itérons sur chacun des k afin de remplir la matrice des noeuds et du résidu 
+    for k in range(N): 
+        if True: # Condition limite du bas 
+            pass 
+        elif True: # Condition limite du haut 
+            pass 
+        elif True: # Condition limite de gauche 
+            pass 
+        elif True: # Condition limite de droite 
+            pass 
+        else: # Alors on est à un noeud qui n'est pas sur le bord 
+            # Évaluons les coefficients des différents noeuds de T, soit Tk+ny, Tk-ny, Tk+1, Tk-1 
+            noeuds += gen_central_values(k, nx, ny)
