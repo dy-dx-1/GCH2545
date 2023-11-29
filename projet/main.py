a776b05b29232d16e8e86a89ea4fbf649b4397e3
dy-dx-1 <nic19sarbe@gmail.com>
2023-11-29 11:46:32 -0500

    quiver test

--- projet/main.py
+++ projet/main.py
@@ -1,5 +1,6 @@
 import fonctions as f 
 import numpy as np 
+import matplotlib.pyplot as plt 
 
 class Parametres():
     u_inf = 1 
@@ -9,11 +10,19 @@ class Parametres():
     theta_min = 0 
     theta_max = 2 * np.pi 
 
+    nx = 5
+    ny = 5
+
 def main():
     # tests, ceci peut etre effacé 
     prm = Parametres()
-    noeuds, res, solutions = f.mdf(r_min=1, r_max=5, theta_min=0, theta_max=6.28, nx=5, ny=5, params=prm)
-    print(solutions)
+    noeuds, res, solutions = f.mdf(params=prm)
+    vr, vtheta = f.vitesses(solutions, prm) 
+    R, THETA = f.gen_maille(prm.R, prm.R_ext, prm.theta_min, prm.theta_max, prm.nx, prm.ny) 
+    vr_mesh = f.arrange_mesh(vr, prm.nx, prm.ny) 
+    vtheta_mesh = f.arrange_mesh(vtheta, prm.nx, prm.ny) 
+    plt.quiver(R, THETA, vr_mesh, vtheta_mesh)
+    plt.show() 
     
 if __name__=="__main__": 
     main() 
