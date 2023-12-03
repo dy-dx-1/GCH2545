0da5d72a1259b34061a0d2e7c54c62a11ca83800
dy-dx-1 <nic19sarbe@gmail.com>
2023-12-03 13:44:26 -0500

    minor reformat

--- projet/fonctions.py
+++ projet/fonctions.py
@@ -18,8 +18,8 @@ def convert_indices(nx:int, i = None, j = None, k = None)->int:
     """
     Prend 2 indices et retourne celui qui n'est pas spécifié selon un maillage de forme 
 
-    k=0  k=1    ... k = ny-1
-    k=ny k=ny+1 ... 
+    k=0  k=1    ... k = nx-1
+    k=nx k=nx+1 ... 
     """
     # on assume que le programmeur sait utiliser la fonction donc pas de check complet des paramètres passés
     if i is None: 
